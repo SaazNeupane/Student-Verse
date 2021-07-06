7331973d80a164f315ab4ef2351b6d8e6b894770
prabeshkhanal <prabesh226@gmail.com>
2021-07-06 11:49:39 +0545

    initial commit

---
+++ app/src/main/java/com/example/studentverse/Signup/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.studentverse.Signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.studentverse.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
