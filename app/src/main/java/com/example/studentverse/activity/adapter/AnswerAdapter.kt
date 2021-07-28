7ead960c665c7f3b53e3222df37d70172e1dcd48
prabeshkhanal <prabesh226@gmail.com>
2021-07-28 15:46:05 +0545

    comment in answers

--- app/src/main/java/com/example/studentverse/activity/adapter/AnswerAdapter.kt
+++ app/src/main/java/com/example/studentverse/activity/adapter/AnswerAdapter.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.studentverse.R
 import com.example.studentverse.activity.model.Answer
+import com.example.studentverse.activity.model.Comment
 
 class AnswerAdapter(
     private val listanswer: ArrayList<Answer>,
@@ -15,6 +18,11 @@ class AnswerAdapter(
 ): RecyclerView.Adapter<AnswerAdapter.AnswerHolder>() {
     class AnswerHolder(view: View) : RecyclerView.ViewHolder(view) {
         val answer: TextView = view.findViewById(R.id.answer)
+        val rvcomments: RecyclerView = view.findViewById(R.id.rvcomments)
+        val tvcomments: TextView = view.findViewById(R.id.tvcomments)
+        val llcomments: LinearLayout = view.findViewById(R.id.llcomments)
+        val lladdcomments: LinearLayout = view.findViewById(R.id.lladdcomment)
+        val tvreply: TextView = view.findViewById(R.id.tvreply)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnswerAdapter.AnswerHolder {
         val view = LayoutInflater.from(parent.context)
@@ -26,6 +34,23 @@ class AnswerAdapter(
         val answer = listanswer[position]
 
         holder.answer.text=answer.text
+        val comments = answer.comment
+        if (comments != null) {
+            holder.tvcomments.setVisibility(View.VISIBLE)
+            holder.tvcomments.setOnClickListener {
+                holder.llcomments.visibility = View.VISIBLE
+                holder.lladdcomments.visibility = View.GONE
+            }
+
+            holder.tvreply.setOnClickListener {
+                holder.llcomments.visibility = View.GONE
+                holder.lladdcomments.visibility = View.VISIBLE
+            }
+        }
+        val commentAdapter = comments?.let { CommentAdapter(it, context) }
+        holder.rvcomments.adapter = commentAdapter
+        holder.rvcomments.layoutManager= LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+
     }
 
     override fun getItemCount(): Int {
